419391fd2ff40ae4723d0c4990700f899ebd1fdb
veroabc <91472809+veroabc@users.noreply.github.com>
2022-08-16 21:18:15 +0200

    Add files via upload

---
+++ dbhtml.php
@@ -0,0 +1,16 @@
+<?php
+	include_once 'includes/dbinit.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+$con;
+
+</body>
+</html>
---
+++ dbinit.php
@@ -0,0 +1,25 @@
+<?php
+
+$host="localhost";
+$port="3306";
+$user="root";
+$password="12345678";
+$dbname="sakila";
+
+$con = new mysqli($host, $port, $user, $password, $dbname);
+//or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+
+$query = "SELECT  user_id, fname FROM  users";
+
+if ($stmt = $con->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($user_id, $fname);
+    while ($stmt->fetch()) {
+        //printf("%s, %s\n", $user_id, $fname);
+    }
+    $stmt->close();
+}
+
+?>
